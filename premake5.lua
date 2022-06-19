6515049db2def4d27f9ba85df3ad840f058bcd3f
Glenn Fiedler <glenn@networknext.com>
2022-06-19 18:41:59 -0400

    initial commit

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+
+solution "proxy"
+	platforms { "portable", "x64", "avx", "avx2" }
+	configurations { "Debug", "Release" }
+	targetdir "bin/"
+	rtti "Off"
+	warnings "Extra"
+	floatingpoint "Fast"
+	flags { "FatalWarnings" }
+	filter "configurations:Debug"
+		symbols "On"
+		defines { "_DEBUG" }
+	filter "configurations:Release"
+		optimize "Speed"
+		defines { "NDEBUG" }
+		editandcontinue "Off"
+	filter "platforms:*x64 or *avx or *avx2"
+		architecture "x86_64"
+
+project "proxy"
+	kind "ConsoleApp"
+	links { "sodium" }
+	files {
+		"proxy.h",
+		"proxy.cpp",
+		"proxy_*.h",
+		"proxy_*.cpp"
+	}
+	filter "system:not windows"
+		links { "pthread" }
+	filter "system:macosx"
+		linkoptions { "-framework SystemConfiguration -framework CoreFoundation" }
